5983fa19089848e46599fd7147ac7818c0217725
Tesniya <157711837+Tesniya@users.noreply.github.com>
2024-07-31 22:32:19 +0530

    Update compress.py

--- compress.py
+++ compress.py
@@ -2,9 +2,9 @@ import pickle
 import bz2
 
 # Load your trained model
-with open('model1.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Compress the model
-with bz2.BZ2File('model1.pkl.bz2', 'wb') as file:
+with bz2.BZ2File('model.pkl.bz2', 'wb') as file:
     pickle.dump(model, file)
